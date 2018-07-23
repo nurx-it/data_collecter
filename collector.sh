7281a2c29eb1d805bd583b4672a0c4eb2cc9d4fb
Alex Sherman <alex.sherman@MacBook-Pro-8.localdomain>
2018-07-23 16:23:16 -0700

    qol update

--- collector.sh
+++ collector.sh
@@ -1,39 +1,29 @@
 #!/bin/bash
-#copies important user files to one directory on admin desktop
 
-if [ $# != 1 ]
-then
-    echo Usage: provide name of user directory to be saved 
-    exit
-fi
-date=`date +%F`
-if [ ! -d /Users/$1 ]
-then
-    echo 'WARNING!'
-    echo 'Username doesnt exist, please enter correct username'
-    read answer
-    if [ ! -d /Users/$answer ]
-    then
-        echo 'Wrong again! Go home - yur drunk'
+echo
+echo !!!IMPORTANT!!! RUN USING SUDO
+echo
+echo Usage: enter the name of the user directory you want zipped
+USERNAME="nul"
+until [ -d /Users/$USERNAME ]
+do
+    if [ $USERNAME == "exit" ]
+        then
+        echo "exiting"
         exit
+        else 
+        echo "Please enter valid user name or exit to escape"
+        read -p "User's name: " USERNAME
     fi
-    mkdir ~/Desktop/$answer_$date
-    cp -R /Users/$answer/Desktop ~/Desktop/$answer_$date
-    cp -R /Users/$answer/Documents ~/Deskstop/$answer_$date
-    chmod -R 755 ~/Desktop/$answer_$date
-    echo 'copy complete'
-    zip -r ~/Desktop/$answer_$date.zip ~/Desktop/$answer_$date
-    echo 'zip complete'
-    rm -r ~/Desktop/$answer_$date
-    echo 'temp files deleted'
-else 
-    mkdir ~/Desktop/$1_$date
-    cp -R /Users/$1/Desktop ~/Desktop/$1_$date
-    cp -R /Users/$1/Documents ~/Desktop/$1_$date
-    chmod -R 755 ~/Desktop/$1_$date
-    echo 'copy complete'
-    zip -r ~/Desktop/$1_$date.zip ~/Desktop/$1_$date
-    echo 'zip complete'
-    rm -r ~/Desktop/$1_$date
-    echo 'temp files deleted'
-fi
+done
+
+date=`date +%F`
+mkdir ~/Desktop/$USERNAME-$date
+cp -R /Users/$USERNAME/Desktop ~/Desktop/$USERNAME-$date
+cp -R /Users/$USERNAME/Documents ~/Desktop/$USERNAME-$date
+chmod -R 755 ~/Desktop/$USERNAME-$date
+echo 'copy complete'
+zip -r ~/Desktop/$USERNAME-$date.zip ~/Desktop/$USERNAME-$date
+echo 'zip complete'
+rm -r ~/Desktop/$USERNAME-$date
+echo 'temp files deleted'
