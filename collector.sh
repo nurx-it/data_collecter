29f16e6010037dc6854173819797c12876b7680b
Alex Sherman <alex.sherman@MacBook-Pro-8.localdomain>
2018-07-24 09:46:43 -0700

    broke out functions

--- collector.sh
+++ collector.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
-echo
-echo !!!IMPORTANT!!! RUN USING SUDO
-echo
-echo Usage: enter the name of the user directory you want zipped
-echo
-until [ -d /Users/$USERNAME ]
-do
-    if [ $USERNAME == "exit" ]
-        then
-        echo "exiting"
-        exit
-        else 
-        echo "Please enter valid user name or exit to escape"
-        read -p "User's name: " USERNAME
-    fi
-done
-date=`date +%F`
-mkdir ~/Desktop/$USERNAME-$date
-cp -R /Users/$USERNAME/Desktop ~/Desktop/$USERNAME-$date
-cp -R /Users/$USERNAME/Documents ~/Desktop/$USERNAME-$date
-chmod -R 755 ~/Desktop/$USERNAME-$date
-echo 'copy complete'
-zip -r ~/Desktop/$USERNAME-$date.zip ~/Desktop/$USERNAME-$date
-echo 'zip complete'
-rm -r ~/Desktop/$USERNAME-$date
-echo 'temp files deleted'
+barker () {
+    echo
+    echo !!!IMPORTANT!!! RUN USING SUDO
+    echo
+    echo Usage: enter the name of the user directory you want zipped
+    echo
+}
+
+prompter () {
+    until [ -d /Users/$USERNAME ]
+    do
+        if [ $USERNAME == "exit" ]
+            then
+            echo "exiting"
+            exit
+            else 
+            echo "Please enter valid user name or exit to escape"
+            read -p "User's name: " USERNAME
+        fi
+    done
+}
+
+collector () {
+    date=`date +%F`
+    mkdir ~/Desktop/$USERNAME-$date
+    cp -R /Users/$USERNAME/Desktop ~/Desktop/$USERNAME-$date
+    cp -R /Users/$USERNAME/Documents ~/Desktop/$USERNAME-$date
+    chmod -R 755 ~/Desktop/$USERNAME-$date
+    echo 'copy complete'
+    zip -r ~/Desktop/$USERNAME-$date.zip ~/Desktop/$USERNAME-$date
+    echo 'zip complete'
+    rm -r ~/Desktop/$USERNAME-$date
+    echo 'temp files deleted'
+}
+
+barker
+prompter
+collector
